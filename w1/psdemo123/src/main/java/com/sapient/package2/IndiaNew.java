717d4280428595b88e0f54c36e93ca6b62860d58
vankadhari sai krishna <vankadhari.saikrishna@publicissapient.com>
2023-03-22 15:49:23 +0530

    w1 22-3-23

---
+++ w1/psdemo123/src/main/java/com/sapient/package2/IndiaNew.java
@@ -0,0 +1,15 @@
+package com.sapient.package2;
+
+import com.sapient.*;
+
+// This class is having default access modifier
+class IndiaNew
+{
+    public static void main(String args[])
+    {
+        // Accessing class Geek from package p1
+        India obj = new India();
+
+        obj.display();
+    }
+}
